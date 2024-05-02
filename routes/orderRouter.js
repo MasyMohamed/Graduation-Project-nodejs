775d5e75e6ed5a21ff79025605bb70dfdb0c6357
MasyMohamed <masym32@gmail.com>
2024-05-02 18:04:22 +0300

    update

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const orderController = require("../controller/order");
 
 router.post("/create", orderController.createOrder);
-router.get("/",orderController.getOrderById);
-router.get("/users",  orderController.getOrdersByUser);
+router.get("/:firebaseId",orderController.getOrderById);
+router.get("/users/:firebaseId",  orderController.getOrdersByUser);
 router.delete("/cancel", orderController.cancelOrder);
 router.post("/checkout", orderController.checkout);
 
